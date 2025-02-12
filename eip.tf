c373f17e028ff3086e6cd463d413e29316e8d772
abhay-kumar10 <82045242+abhay-kumar10@users.noreply.github.com>
2025-02-12 19:24:30 +0530

    Update eip.tf
    ec2 policy check

--- eip.tf
+++ eip.tf
@@ -1,7 +1,9 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "us-west-2"
 }
 
-resource "aws_eip" "lb" {
-  domain   = "vpc"
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+
 }
